48b9e36a83341d24ca90c0c31dcb8eb5ff6afd5a
Binal Patel <147269633+Binalpatel1012@users.noreply.github.com>
2024-01-27 20:50:30 +0000

    Create a method named Subtract, and ensure that it accepts a get request and two decimal parameters.

--- WebAPI/Calculatorcontroller.cs
+++ WebAPI/Calculatorcontroller.cs
@@ -7,10 +7,18 @@ namespace MyApp.Namespace
     [ApiController]
     public class CalculatorController : ControllerBase
     {
+        //Add two decimal parameters.
         [HttpGet("Add")]
         public ActionResult<decimal> Add(decimal a, decimal b)
         {
             return a + b;
+        }
+
+        //Subtract two decimal parameters.
+        [HttpGet("Subtract")]
+        public ActionResult<decimal> Subtract(decimal a, decimal b)
+        {
+            return a - b;
         }
 
     }
