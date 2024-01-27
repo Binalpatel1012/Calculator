4e4ee77be885461be85ab0e7834b8af7e6172328
Binal Patel <147269633+Binalpatel1012@users.noreply.github.com>
2024-01-27 21:03:19 +0000

    Create a method named Modulo, and ensure that it accepts a get request and two decimal parameters.

--- WebAPI/Calculatorcontroller.cs
+++ WebAPI/Calculatorcontroller.cs
@@ -7,38 +7,48 @@ namespace MyApp.Namespace
     [ApiController]
     public class CalculatorController : ControllerBase
     {
-        //Add two decimal parameters.
+        //Create a method named Add, and ensure that it accepts a get request and Add two decimal parameters.
         [HttpGet("Add")]
         public ActionResult<decimal> Add(decimal a, decimal b)
         {
             return a + b;
         }
 
-        //Subtract two decimal parameters.
+        //Create a method named Subtract, and ensure that it accepts a get request and Add two decimal parameters.
         [HttpGet("Subtract")]
         public ActionResult<decimal> Subtract(decimal a, decimal b)
         {
             return a - b;
         }
 
-        //Multiply two decimal parameters.
+        //Create a method named Multiply, and ensure that it accepts a get request and two decimal parameters.
         [HttpGet("Multiply")]
         public ActionResult<decimal> Multiply(decimal a, decimal b)
         {
             return a * b;
         }
 
-        //Divide two decimal parameters.
+        //Create a method named Divide, and ensure that it accepts a get request and two decimal parameters.
         [HttpGet("Divide")]
         public ActionResult<decimal> Divide(decimal a, decimal b)
         {
             if (b == 0)
             {
-                return BadRequest("Division by zero is not allowed.");
+                return BadRequest("Division by zero is not allowed!");
             }
             return a / b;
         }
 
-        
+        //Create a method named Modulo, and ensure that it accepts a get request and two decimal parameters.
+        [HttpGet("Modulo")]
+        public ActionResult<decimal> Modulo(decimal a, decimal b)
+        {
+            if (b == 0)
+            {
+                return BadRequest("Modulo by zero is not allowed!");
+            }
+            return a % b;
+        }
+
     }
 }
